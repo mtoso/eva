aeecb9c6d32cbe0e87502b76dc3e335b9feb306b
Mattia Toso <mtoso@netflix.com>
2020-03-22 18:04:28 -0700

    Add lambda functions

--- src/Eva.ts
+++ src/Eva.ts
@@ -67,17 +67,26 @@ export class Eva {
         }
 
         // Function declaration: (def square (x) (* x x))
+        //
+        // Syntatic sugar for: (var square (lambda (x) (* x x)))
         if (exp[0] === 'def') {
             const [_tag, name, params, body] = exp;
-            const fn = {
-                params,
-                body,
-                env // closure
-            };
-            // install the fn in the env
-            return env.define(name, fn);
+
+            // JIT-transpile to a variable declaration
+            const varExp = ['var', name, ['lambda', params, body]];
+            
+            return this.eval(varExp, env);
         }
 
+        // Lambda function: (lambda (x) (* x x))
+        if (exp[0] === 'lambda') {
+            const [_tag, params, body] = exp;
+            return {
+                params,
+                body,
+                env, // Closure
+            }
+        }
 
         // Function calls:
         //
